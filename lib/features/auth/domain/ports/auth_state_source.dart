c2720a1900501b81c884b7271f7201e04d09f91a
pierre <pierre.fache@never2wait.com>
2025-08-27 18:18:40 +0200

    refacto

--- lib/features/auth/domain/ports/auth_state_source.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:volleyapp/features/auth/domain/entitites/auth_user.dart';
-
-abstract class AuthStateSource {
-  Stream<AuthUser?> get changes;
-  Option<AuthUser> getCurrentUser();
-}
